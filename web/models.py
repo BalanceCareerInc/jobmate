5ccee86777ca502f7f01ee3c2384f3154ad0a04c
acuros <acuros@kaist.ac.kr>
2015-01-01 23:50:38 -0800

    Add fixture, user

--- web/models.py
+++ web/models.py
@@ -1,11 +1,16 @@
-from bynamodb.attributes import StringAttribute, NumberAttribute
+from bynamodb.attributes import StringAttribute, NumberAttribute, ListAttribute
 from bynamodb.model import Model
 
 
 class Coordinate(Model):
 
-    __fixtures__ = 'universities.json',
+    __fixtures__ = 'universities.json', 'open_recruitment_exp.json'
 
     name = StringAttribute(hash_key=True)
     group = StringAttribute()
     value = NumberAttribute()
+
+
+class User(Model):
+    username = StringAttribute(hash_key=True)
+    coordinates = ListAttribute()
