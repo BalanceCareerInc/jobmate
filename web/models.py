0aa3cd419e9b5ae29be9cc0b43e8e198c67b7882
acuros <acuros@kaist.ac.kr>
2015-01-01 19:28:02 -0800

    Set fixtures

--- web/models.py
+++ web/models.py
@@ -3,6 +3,9 @@ from bynamodb.model import Model
 
 
 class Coordinate(Model):
+
+    __fixtures__ = 'universities.json',
+
     name = StringAttribute(hash_key=True)
     group = StringAttribute()
     value = NumberAttribute()
