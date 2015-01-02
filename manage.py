0558179c133853d38c85d574e45c33c8aa87474c
acuros <acuros@kaist.ac.kr>
2015-01-01 19:27:57 -0800

    Load fixture on initdb

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # -*-coding:utf8-*-
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('command', choices=['initdb'])
@@ -8,9 +9,15 @@ args = parser.parse_args()
 
 def init_db():
     import inspect
+    import json
     from boto.dynamodb2.layer1 import DynamoDBConnection
     from bynamodb.model import Model
     from web import models
+
+    def load_fixture(model, fixture):
+        with open(os.path.join('fixtures', fixture), 'r') as f:
+            for row in json.loads(f.read()):
+                model.put_item(**row)
 
     conn = DynamoDBConnection()
     table_names = conn.list_tables()['TableNames']
@@ -20,6 +27,7 @@ def init_db():
     for model in models_:
         if model.get_table_name() not in table_names:
             model.create_table(2, 2)
+            [load_fixture(model, fixture) for fixture in getattr(model, '__fixtures__', [])]
 
 
 if args.command == 'initdb':
