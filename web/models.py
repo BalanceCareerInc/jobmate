ef8e21dc46fe24ede061a51a3a63e5347548fbac
acuros <acuros@kaist.ac.kr>
2015-01-02 03:22:53 -0800

    Normalize names

--- web/models.py
+++ web/models.py
@@ -4,7 +4,7 @@ from bynamodb.model import Model
 
 class Coordinate(Model):
 
-    __fixtures__ = 'universities', 'recruit_exp'
+    __fixtures__ = 'university', 'recruit_exp'
 
     name = StringAttribute(hash_key=True)
     group = StringAttribute()
