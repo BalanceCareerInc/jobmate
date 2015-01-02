a611588fb8f8d4c78e5f25d1ba92ed7258c17a3b
acuros <acuros@kaist.ac.kr>
2015-01-01 21:36:30 -0800

    Load config, patch dynamodb

--- web/app.py
+++ web/app.py
@@ -1,9 +1,19 @@
+import os
+
 from flask import Flask
 from flask.ext.compress import Compress
+
+from bynamodb import patch_dynamodb_connection
 
 
 def create_app(config_file='baseconfig.py'):
     app = Flask(__name__)
+    config_file = os.path.join(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0], config_file)
     app.config.from_pyfile(config_file)
     Compress(app)
+    patch_dynamodb_connection(
+        host=app.config['DYNAMODB_HOST'],
+        port=app.config['DYNAMODB_PORT'],
+        is_secure=app.config['DYNAMODB_IS_SECURE'],
+    )
     return app
