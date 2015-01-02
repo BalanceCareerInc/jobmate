87cea3267b59dddff0cbaa3330716180ae77d443
acuros <acuros@kaist.ac.kr>
2015-01-01 20:35:15 -0800

    create_app

--- web/app.py
+++ web/app.py
@@ -1,1 +1,9 @@
-from flask import app
+from flask import Flask
+from flask.ext.compress import Compress
+
+
+def create_app(config_file='baseconfig.py'):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    Compress(app)
+    return app
