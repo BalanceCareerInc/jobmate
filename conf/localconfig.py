1d04e58f09868c7dd7f1ba562b7fb5fed002b815
acuros <acuros@kaist.ac.kr>
2015-01-06 20:36:59 +0900

    Ignores localconfig

--- conf/localconfig.py
+++
@@ -1,9 +0,0 @@
-# -*-coding:utf8-*-
-DEBUG = True
-
-DYNAMODB_HOST = 'localhost'
-DYNAMODB_PORT = '8000'
-DYNAMODB_IS_SECURE = False
-
-SESSION_COOKIE_NAME = 'jobmate_session'
-REDIS_HOST = 'localhost'
