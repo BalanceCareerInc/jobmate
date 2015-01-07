6f1ed6f3946ebdb36bb2d35905405aa40747e738
acuros <acuros@kaist.ac.kr>
2015-01-07 13:01:11 +0900

    Member blueprint registered

--- web/app.py
+++ web/app.py
@@ -1,5 +1,7 @@
 import os
 
+import member
+
 from flask import Flask
 from flask.ext.compress import Compress
 
@@ -9,13 +11,25 @@ from web.session import RedisSessionInterface
 
 def create_app(config_file='baseconfig.py'):
     app = Flask(__name__)
-    config_file = os.path.join(os.getcwd(), 'conf', config_file)
-    app.config.from_pyfile(config_file)
+    app.config.from_pyfile(os.path.join(os.getcwd(), 'conf', config_file))
+
+    app.register_blueprint(member.bp, url_prefix='/member')
+
     Compress(app)
+
     patch_dynamodb_connection(
         host=app.config['DYNAMODB_HOST'],
         port=app.config['DYNAMODB_PORT'],
         is_secure=app.config['DYNAMODB_IS_SECURE'],
     )
+
     app.session_interface = RedisSessionInterface(app.config['REDIS_HOST'])
+
+    @app.after_request
+    def allow_access_control(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'content-type,token'
+        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
+        return response
+
     return app
