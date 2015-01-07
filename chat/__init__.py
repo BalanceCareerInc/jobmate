b7baab4b4828b06420536841ed7e16f26619cd84
acuros <acuros@kaist.ac.kr>
2015-01-07 21:40:36 +0900

    Logserver added

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,1 +1,11 @@
 # -*-coding:utf8-*-
+
+
+def get_config(config_file):
+    with open('conf/%s' % config_file, 'r') as f:
+        data = f.read()
+    config = dict()
+    default = dict()
+    exec '' in default
+    exec data in config
+    return dict((k, v) for k, v in config.iteritems() if k not in default)
