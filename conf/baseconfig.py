c7bbd530e0c0d08313f68e9c802799fdb646fbb4
acuros <acuros@kaist.ac.kr>
2015-01-13 10:58:43 +0900

    APN_ARN added at settings

--- conf/baseconfig.py
+++ conf/baseconfig.py
@@ -4,3 +4,5 @@ DEBUG = False
 SECRET_KEY = '\xe69jZ\x9d&\xc6\xb7\x90\xde\xd3|}j\x08\xbeiZZ\xcb-.\xe5\xa1\x08fv\x8d\xb8#t\xd0'
 SESSION_COOKIE_NAME = 'jobmate_session'
 REDIS_HOST = 'localhost'
+
+APP_ARN = 'arn:aws:sns:ap-northeast-1:357340406057:app/GCM/Jobmate'
