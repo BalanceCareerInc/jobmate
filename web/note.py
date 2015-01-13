d42c15afe2fa82eef53e9946d660163f775c1da7
acuros <acuros@kaist.ac.kr>
2015-01-13 23:11:31 +0900

    delete_note added, proper response code

--- web/note.py
+++ web/note.py
@@ -1,35 +1,14 @@
 # -*-coding:utf8-*-
+import json
 import uuid
 from bynamodb.exceptions import ItemNotFoundException
 from flask import Blueprint, request, jsonify, abort
 import time
-from models import Note, Comment
+from models import Note, Comment, DeletedArchive
 from web.decorators import login_required
 from web.utils import jsonable
 
 bp = Blueprint('note', __name__)
-
-
-@bp.route('/', methods=['POST'])
-@login_required
-def write_note():
-    required_params = 'title', 'content', 'is_secret'
-    if any([param not in request.json for param in required_params]):
-        return abort(400)
-
-    note = Note.put_item(
-        id=uuid.uuid1(),
-        writer=request.user.id,
-        published_at=time.time(),
-        title=request.json['title'],
-        content=request.json['content']
-    )
-
-    pair = request.user.pair
-    pair.note_ids.append(note.id)
-    pair.save()
-
-    return jsonify(id=note.id)
 
 
 @bp.route('/notes', methods=['GET'])
@@ -54,6 +33,48 @@ def get_notes():
     u1_notes = Note.query(index_name='WriterIndex', writer_id=u1)
     u2_notes = Note.query(index_name='WriterIndex', writer_id=u2)
     return jsonify(notes=merge(u1_notes, u2_notes))
+
+
+@bp.route('/', methods=['POST'])
+@login_required
+def write_note():
+    required_params = 'title', 'content', 'is_secret'
+    if any([param not in request.json for param in required_params]):
+        return abort(400)
+
+    note = Note.put_item(
+        id=uuid.uuid1(),
+        writer=request.user.id,
+        published_at=time.time(),
+        title=request.json['title'],
+        content=request.json['content']
+    )
+
+    pair = request.user.pair
+    pair.note_ids.append(note.id)
+    pair.save()
+
+    return jsonify(id=note.id), 201
+
+
+@bp.route('/<path:note_id>', methods=['DELETE'])
+@login_required
+def delete_note(note_id):
+    try:
+        note = Note.get_item(note_id)
+    except ItemNotFoundException:
+        return abort(404)
+
+    if note.writer_id != request.user.id:
+        return abort(403)
+
+    DeletedArchive.put_item(
+        type='NOTE',
+        deleted_at=time.time(),
+        owner_id=request.user.id,
+        data=json.dumps(jsonable(note))
+    )
+    return '', 204
 
 
 @bp.route('/<path:note_id>', methods=['GET'])
@@ -87,4 +108,4 @@ def write_comment(note_id):
         writer_id=request.user.id,
         content=content
     )
-    return jsonify(note_id=note.id, published_at=float(comment.published_at))
+    return jsonify(note_id=note_id, published_at=comment.published_at), 201
