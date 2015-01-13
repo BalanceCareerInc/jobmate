9ed3fa5f7659973136b154b9773b7c072162c16c
acuros <acuros@kaist.ac.kr>
2015-01-13 13:34:43 +0900

    Register device token on SNS

--- web/member.py
+++ web/member.py
@@ -1,7 +1,8 @@
 # -*-coding:utf8-*-
 from uuid import uuid1
+from boto import sns
 
-from flask import Blueprint, request, abort, jsonify
+from flask import Blueprint, request, abort, jsonify, current_app
 
 from models import Coordinate, User
 
@@ -17,12 +18,18 @@ def register():
             return None
         params.update(looking_for_now=True, orientation='etc')
         return params
+
     cleaned_params = clean(request.json)
     if cleaned_params is None:
         return abort(400)
-    user = User.put_item(
-        username=str(uuid1()),
+    user = User(
+        id=str(uuid1()),
         gender=cleaned_params.pop('gender'),
+        device_token=cleaned_params.pop('device_token'),
         matching_info=cleaned_params,
     )
-    return jsonify(username=user.username)
+    conn = sns.connect_to_region('ap-northeast-1')
+    response = conn.create_platform_endpoint(current_app.config['APP_ARN'], user.device_token, user.id)
+    user.endpoint_arn = response['CreatePlatformEndpointResponse']['CreatePlatformEndpointResult']['EndpointArn']
+    user.save()
+    return jsonify(username=user.id)
